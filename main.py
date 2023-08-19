2fddebb8cc61ca9faef2accc37a8bfe6dc5a164e
heysen3101 <heysenpinto1996.hp@gmail.com>
2023-08-19 15:14:16 -0500

    initial commit- 6

--- main.py
+++ main.py
@@ -1,148 +1,180 @@
-import matplotlib.pyplot as plt
+''''''
+#Importing Librabries Needed for this Project
 import pandas as pd
-#import data
-data = pd.read_csv("C:/Users/heyse/OneDrive/Desktop/portfolio projects/NetflixPythonProject/netflix_titles.csv")
-print(data)
-
-#head
-#print(data.head())
-
-#tail
-#print(data.tail())
-
-#shape
-#print(data.shape)
-
-#size
-#print(data.size)
-
-#column names
-#print(data.columns)
-
-#datatypes
-#print(data.dtypes)
-
-#datainformation
-#print(data.info())
-
-#print(data.shape)
-
-##TASK 1= ARE THERE ANY DUPLICATE VALUES? IF YES REMOVE THE DUPLICATE VALUES.
-#find out the duplicate data
-print(data[data.duplicated()])
-
-#remove the duplicate data
-print(data.drop_duplicates())
-
-#to make the changes permanent add inplace=true
-data.drop_duplicates(inplace = True)
-
-### TASK 2 = IS THERE ANY NULL VALUE? IF ANY , show them on heat map
-
-#show the null values
-print(data.isnull())
-
-#show the sum of null values for each column
-print(data.isnull().sum())
-
-#show on heat map(Seaborn Library)
+import matplotlib.pyplot as plt
 import seaborn as sns
-#sns.heatmap(data.isnull())
-#plt.show()
+''''''
 
-#1)show the show id and director for 'House of Cards'
-#using isin function
-print(data[data['title'].isin(['House of Cards'])])
-row= data.iloc[1059]  # DataFrame is 0-indexed, so 99 represents the 100th row
+''''''
+#Loading and Reading the Data
+data = pd.read_csv("C:/Users/heyse/OneDrive/Desktop/portfolio projects/NetflixPythonProject/netflix_titles.csv")
+''''''
+
+''''''
+#Formatting the Column names (using capitalize function)
+data.columns = data.columns.str.capitalize()
+''''''
+
+''''''
+#Baic Functions used in Python
+
+#print(data.columns) #To view the columns in the dataset
+
+#print(data.head()) #To view the first 5 datainfo
+
+#print(data.tail()) #To view the last 5 datainfo
+
+#print(data.shape) #To view the shape of the table
+
+#print(data.size) #How many cells are used in the Data
+
+#print(data.dtypes) #Data type eg:float,object,int,datetime
+
+#print(data.info())  #gives general information about the dataset
+''''''
+
+''''''
+#Formatting the dataset by checking the duplicates and removing the duplicates
+print(data[data.duplicated()]) # checking the duplicates
+
+print(data.drop_duplicates()) # removing the duplicates
+
+data.drop_duplicates(inplace = True) #permanent changes to the dataset
+''''''
+
+''''''
+#Checking The null values
+print(data.isnull().sum()) #null values for each column
+
+#sns.heatmap(data.isnull()) #plot the null values on heatmap
+#plt.show()
+''''''
+
+''''''
+# Show the director and Show id for 'Cuckoo'
+print(data[data['Title'].isin(['Cuckoo'])]) #using isin function
+row= data.iloc[514]  # iloc function shows all the information in that row
 print(row)
 
-# using str.contains
-print(data[data['title'].str.contains('House of Cards')])
+print(data[data['Title'].str.contains('Cuckoo')]) #using contains function for same result
+''''''
 
-#2) which year had the highest number of releases?
-#convert int64 into str
-data['release_year'] = data['release_year'].astype(str)
-#check the data types and convert it from object to date and time(use date time function)
-data['Date_N'] = pd.to_datetime(data['release_year'].str.strip(), format='%Y', errors='coerce')
-#dhdmh
-#check the number of releases in that year
-print(data['Date_N'].dt.year.value_counts())
+''''''
+# Show the number of releases for every year
+#Formatting the existing Date_added column. (datetime function)
+data['DateTime_Added'] = pd.to_datetime(data['Date_added'].str.strip(), format='%B %d, %Y', errors='coerce')
+data['New_Date_Added'] = data['DateTime_Added'].dt.date
 
-#show this on a bar graph
-#data['Date_N'].dt.year.value_counts().plot(kind='bar')
+#Formatting the existing Release_year column.
+data['Release_Year_A'] = pd.to_datetime(data['Release_year'], format='%Y', errors='coerce')
+
+print(data['Release_Year_A'].dt.year.value_counts()) #count of total releases for each year
+
+#Representation on the bar graph
+#data['Release_Year_A'].dt.year.value_counts().plot(kind='bar')
 #plt.show()
+''''''
 
-print(data.columns)
+''''''
+#How many movies and shows are there in the dataset? Show it with barplot
 
-#3) How many movies and shows are there in the dataset? show with count plot
-#use group by function to proceed
-print(data.groupby('type').type.count())
+print(data.groupby('Type').Type.count()) #used group by function to categorise
 
-#show on pie chart
-#data['Category'] = data['Category'].str.strip()
-#sns.countplot(data=data['Category'])
-#plt.show()
-category_counts = data['type'].value_counts()
+#show on barplot
+sns.set(style="ticks")
+plt.figure(figsize=(8, 6))
+type_counts = data['Type'].value_counts()
+# seaborn
+sns.barplot(x=type_counts.index, y=type_counts.values)
+plt.xlabel('Type')
+plt.ylabel('Count')
+plt.title('Distribution of Types')
+plt.show()
+''''''
 
-# Create a count plot using the counts
-#sns.barplot(x=category_counts.index, y=category_counts.values)
-#plt.show()
+''''''
+#Show all the movies released in year 2017
+print(data[(data['Type']=='Movie') & (data['Release_Year_A']=='2017')]) #And function
 
-#4 SHow all the movies that were release in 2000
-#filtering
-print(data[(data['type']=='Movie') & (data['Date_N']=='2000')])
+#Show only the titles of tv shows where all tv shows were release in india
+print(data[(data['Type']=='TV Show') & (data['Country']=='India')]['Title'])
+''''''
 
-#5 Show only the titles of tv shows where all tv shows were release in india
-print(data[(data['director']=='TV Show') & (data['country']=='India')]['title'])
+''''''
+#Show Top 10 Directors who gave highest number of tv shows& movies to Netflix
 
-#6 Show top 10 directors, who gave highest number of tv shows& movies to Netflix.
-#value_count()functions used
+print(data['Director'].value_counts().head(10)) #value_count function
+''''''
 
-print(data['director'].value_counts().head(10))
+''''''
+# show all the Records where TYPE is movie and LISTED is comedies or country is united kingdom
+# Filtering using functions(and,or operators)
+print(data[(data['Type']=='Movie') & (data['Listed_in']=='Comedies')])
+#and or function
+print(data[(data['Type']=='Movie') & (data['Release_Year_A']=='2000') |(data['Country']=='United Kingdom')])
+''''''
 
-#7 show all the Records where category is movie and type is comedies or country is united kingdom
-# filtering using functions(and,or operators)
-print(data[(data['type']=='Movie') & (data['listed_in']=='Comedies')])
-#addid or function
-print(data[(data['type']=='Movie') & (data['Date_N']=='2000') |(data['country']=='United Kingdom')])
-
-#8 in how many movies , tom cruise was cast
-#we should use contain function but before we should remove null values
+''''''
+# How many movies was Tom Cruise casted in?
 #drop na function
 data_new=data.dropna()
-
 print(data_new.isnull().sum())
 
-#use contain function to show movies where tom cruise is included
-print(data_new[data_new['cast'].str.contains('Tom Cruise')])
+print(data_new[data_new['Cast'].str.contains('Tom Cruise')]) #used contain function to show movies where tom cruise is included
+''''''
 
-#9 what are different ratings defined by netflix
-#use nunique() function to show different ratings
+''''''
+#List all different ratings defined by netflix
+print(data['Rating'].nunique()) #use nunique() function to show different ratings
 
-print(data['rating'].nunique())
+print(data['Rating'].unique()) #use unique to show the names of differnt ratings
 
-#use unique to show the names of differnt ratings
-print(data['rating'].unique())
+#List all movies got TV-14 rating in Canada
+print(data[(data['Type']=='Movie') & (data['Rating']=='TV-14') &(data['Country']=='Canada')])
+print(data[(data['Type']=='Movie') & (data['Rating']=='TV-14') &(data['Country']=='Canada')].shape)
 
-#9.1 how many movies got TV-14 rating in canada
-print(data[(data['type']=='Movie') & (data['rating']=='TV-14') &(data['country']=='Canada')])
+#How many tv show got r rating after 2018
+print(data[(data['Type']=='TV Show') & (data['Rating']=='R') &(data['Release_Year_A']>'2015')])
+print(data[(data['Type']=='TV Show') & (data['Rating']=='R') &(data['Release_Year_A']>'2015')].shape)
+''''''
 
-print(data[(data['type']=='Movie') & (data['rating']=='TV-14') &(data['country']=='Canada')].shape)
+''''''
+#What is the maximum duration of a movie show on netflix
 
-#9.2 how many tv show got r rating after 2018
-print(data[(data['type']=='TV Show') & (data['rating']=='R') &(data['Date_N']>'2018')])
-print(data[(data['type']=='TV Show') & (data['rating']=='R') &(data['Date_N']>'2018')].shape)
+print(data.Duration.unique()) #there are different durations, use unique to find it
 
-#Q 10 What is the maximum duration of a movie show on netflix
-
-#there are different durations, use unique to find it
-print(data.duration.unique())
-
-#convert object to numeric use split with new colums
-data[['minutes','unit']]=data['duration'].str.split(' ',expand=True)
+data[['minutes','unit']]=data['Duration'].str.split(' ',expand=True) #convert object to numeric use split with new colums
 print(data)
 
 #use max function to find highest duration
-data['minutes'].max
+data['minutes'] = pd.to_numeric(data['minutes']) # Convert 'minutes' column to numeric
 
+max_minutes = data['minutes'].max() # Find the highest value in the 'minutes' column
+print("The highest value in the 'minutes' column is:", max_minutes) # Print the highest value
 
+min_minutes = data['minutes'].min() #minimum minutes
+print("The lowest value in the 'minutes' column is:", min_minutes)
+
+mean_minutes = data['minutes'].mean() #mean value
+print("The mean value in the 'minutes' column is:", mean_minutes)
+''''''
+
+''''''
+#Which individual country has the highest no of tv shows?
+data_tvshow= data[data['Type']=='TV Show']
+print(data_tvshow.Country.value_counts().head(1))
+''''''
+
+''''''
+#Sort the dataset by year? use sort function
+print(data.sort_values(by='Release_year', ascending=False).head(5))
+''''''
+
+''''''
+#Find all the instances where Type is movie and type is dramas |OR| Tpe is tv show and type is Listed_in TV
+#print(data[(data['Type']=='Movie') & (data['Listed_in']=='Dramas')])
+
+print(data[(data['Type']=='TV Show') & (data['Listed_in']=="Kids' TV")])
+
+print(data[(data['Type']=='Movie') & (data['Listed_in']=='Dramas')|(data['Type']=='TV Show') & (data['Listed_in']=="Kids' TV")])
+''''''
